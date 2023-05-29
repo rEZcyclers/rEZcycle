d1ec137197e844367e780704d74bb678ef3eb16e
J-hta-n <103252521+J-hta-n@users.noreply.github.com>
2023-05-29 15:46:24 +0800

    Delete react-rEZcycle directory

--- react-rEZcycle/src/components/ItemManager.tsx
+++
@@ -1,9 +0,0 @@
-function ItemManager() {
-  return (
-    <>
-      <h1>Item Manager</h1>
-    </>
-  );
-}
-
-export default ItemManager;
--- react-rEZcycle/src/supabase.tsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// rEZcycle supabase
-export const supabase = createClient(
-  "https://rtgficcuqderxusnmkkh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Z2ZpY2N1cWRlcnh1c25ta2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUwNzI0NjcsImV4cCI6MjAwMDY0ODQ2N30.2L7pCi3tu8PRoDRFeCFvS6KPEIqxLi9OqVcVVv-ZtFk"
-);
