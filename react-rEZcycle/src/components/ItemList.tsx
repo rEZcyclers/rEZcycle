d9d67952f9382af31185c7de731ba89ceded0236
J-hta-n <e0969121@u.nus.edu>
2023-05-29 11:24:12 +0800

    Template Web App

---
+++ react-rEZcycle/src/components/ItemList.tsx
@@ -0,0 +1,11 @@
+import { Checkbox, Card, CardContent, Typography, Stack } from "@mui/material";
+
+function ItemList() {
+  return (
+    <>
+      <h1>ItemList</h1>
+    </>
+  );
+}
+
+export default ItemList;
