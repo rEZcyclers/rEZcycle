ac5d459f404840c0b3501ce5438037ed2d21808d
J-hta-n <e0969121@u.nus.edu>
2023-05-31 13:54:04 +0800

    Add comment

--- react-rEZcycle/src/supabase.ts
+++ react-rEZcycle/src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// rEZcycle supabase for testing
+// supabase account for testing
 export const supabase = createClient(
   "https://rtgficcuqderxusnmkkh.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Z2ZpY2N1cWRlcnh1c25ta2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUwNzI0NjcsImV4cCI6MjAwMDY0ODQ2N30.2L7pCi3tu8PRoDRFeCFvS6KPEIqxLi9OqVcVVv-ZtFk"
