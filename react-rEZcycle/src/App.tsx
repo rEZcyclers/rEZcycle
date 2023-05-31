5dfa48ea0e857425ef5ab1ff00ec3d2c9efc63d6
J-hta-n <e0969121@u.nus.edu>
2023-05-31 12:23:53 +0800

    Add react brower router to App.tsx

--- react-rEZcycle/src/App.tsx
+++ react-rEZcycle/src/App.tsx
@@ -1,36 +1,69 @@
-import { useEffect, useState } from "react";
-// import "./App.css";
-import { supabase } from "./supabase";
-import { Session } from "@supabase/supabase-js";
-import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
-import LoginScreen from "./components/LoginScreen";
-import HomeScreen from "./components/HomeScreen";
-import SideBar from "./components/SideBar";
+import { CssBaseline, createTheme } from "@mui/material";
 import Base from "./components/Base";
+import HomePage from "./pages/HomePage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
+import { createContext, useEffect, useState } from "react";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "./supabase";
+import HistoryPage from "./pages/HistoryPage";
+import FriendsPage from "./pages/FriendsPage";
+import CalendarPage from "./pages/CalendarPage";
 
-const theme = createTheme({
-  palette: {
-    background: {
-      default: "#FCFFFA",
-    },
+// const theme = createTheme({
+//   palette: {
+//     background: {
+//       default: "#FCFFFA",
+//     },
+//   },
+// });
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
   },
-});
+  {
+    path: "profile",
+    element: <ProfilePage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "history",
+    element: <HistoryPage />,
+  },
+  {
+    path: "friends",
+    element: <FriendsPage />,
+  },
+  {
+    path: "Calendar",
+    element: <CalendarPage />,
+  },
+]);
+
+// For handling loginStatus
+export const loginContext = createContext<Session | null>(null);
 
 function App() {
-  const [session, setSession] = useState<Session | null>(null);
+  const [loginStatus, setLoginStatus] = useState<Session | null>(null);
 
   useEffect(() => {
     const subscription = supabase.auth.onAuthStateChange((event, session) => {
-      setSession(session);
+      setLoginStatus(session);
     });
+    console.log("useEffect called");
     return () => subscription.data.subscription.unsubscribe();
-  });
+  }, [loginStatus]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {session ? <Base /> : <LoginScreen />}
-    </ThemeProvider>
+    <loginContext.Provider value={loginStatus}>
+      <RouterProvider router={router} />
+    </loginContext.Provider>
   );
 }
 
