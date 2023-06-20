426b053f7d2c42d5f26a79696dea0926cc6ec7d9
jh <e0969121@u.nus.edu>
2023-06-21 00:17:04 +0800

    Preliminary ResultsPage functionality

--- client/src/components/ResultsPage.tsx
+++ client/src/components/ResultsPage.tsx
@@ -1,15 +1,17 @@
 import { useContext, useEffect } from "react";
+import { backendContext } from "../App";
 import {
   DonatableItem,
   EWasteItem,
   RecyclableItem,
   DonateLocation,
   RepairLocation,
-  backendContext,
-} from "../App";
+  DDLoc,
+  DRLoc,
+  EDLoc,
+  ERLoc,
+} from "../DataTypes";
 import { Box, Button, Stack, Typography } from "@mui/material";
-import { supabase } from "../supabase";
-import { ReceiptLong } from "@mui/icons-material";
 
 type Condition = "Good" | "Repairable" | "Spoilt" | "";
 interface Props {
@@ -32,8 +34,17 @@ function ResultsPage({
   setStage,
 }: Props) {
   // Retrive raw data first
-  const { recyclablesData, donatablesData, eWasteData } =
-    useContext(backendContext);
+  const {
+    recyclablesData,
+    donatablesData,
+    eWasteData,
+    donateLocData,
+    repairLocData,
+    DDLocData,
+    DRLocData,
+    EDLocData,
+    ERLocData,
+  } = useContext(backendContext);
 
   let recyclablesResults: RecyclableItem[] = []; // Selected recyclables which can be disposed in blue bins
   let unrecyclablesResults: RecyclableItem[] = []; // Selected recyclables which CANNOT be disposed in blue bins
@@ -68,7 +79,6 @@ function ResultsPage({
     //       item["bluebin_eligibility"] != 0;
     //   }
     // );
-
     unrecyclablesResults = selectedItems[0]
       .map((sel, i) => (sel ? i : -1))
       .filter(
@@ -77,56 +87,62 @@ function ResultsPage({
           (recyclablesData[i]["bluebin_eligibility"] === 0 ||
             !recyclableConditions[i])
       )
-      .map((i) => recyclablesData(i));
+      .map((i) => recyclablesData[i]);
 
     goodDonatables = donatableConditions
       .map((cond, i) => (cond === "Good" ? i : -1))
       .filter((i) => i != -1)
       .map((i) => donatablesData[i]);
-
     repairDonatables = donatableConditions
       .map((cond, i) => (cond === "Repairable" ? i : -1))
       .filter((i) => i != -1)
       .map((i) => donatablesData[i]);
-
     spoiltDonatables = donatableConditions
       .map((cond, i) => (cond === "Spoilt" ? i : -1))
       .filter((i) => i != -1)
       .map((i) => donatablesData[i]);
-
-    goodDonatablesResults = goodDonatables.map(async (item: DonatableItem) => {
-      const { data, error } = await supabase
-        .from("DonatablesDonateLocations")
-        .select()
-        .match({ donatable_id: item["donatable_id"] });
-      const locations =
-        data === null
-          ? []
-          : data.map(async (entry) => {
-              await supabase
-                .from("donateLocations")
-                .select()
-                .match({ donate_id: entry["donate_id"] });
-            });
-      return locations;
+    goodDonatablesResults = goodDonatables.map((item: DonatableItem) => {
+      return DDLocData.filter(
+        (entry: DDLoc) => entry["donatable_id"] === item["donatable_id"]
+      ).map((entry: DDLoc) => {
+        return donateLocData[entry["donate_id"] - 1];
+      });
+    });
+    repairDonatablesResults = repairDonatables.map((item: DonatableItem) => {
+      return DRLocData.filter(
+        (entry: DRLoc) => entry["donatable_id"] === item["donatable_id"]
+      ).map((entry: DRLoc) => {
+        return repairLocData[entry["repair_id"] - 1];
+      });
     });
 
     goodEWaste = eWasteConditions
       .map((cond, i) => (cond === "Good" ? i : -1))
       .filter((i) => i != -1)
       .map((i) => eWasteData[i]);
-
     repairEWaste = eWasteConditions
       .map((cond, i) => (cond === "Repairable" ? i : -1))
       .filter((i) => i != -1)
       .map((i) => eWasteData[i]);
-
     spoiltEWaste = eWasteConditions
       .map((cond, i) => (cond === "Spoilt" ? i : -1))
       .filter((i) => i != -1)
       .map((i) => eWasteData[i]);
+    goodEWasteResults = goodEWaste.map((item: EWasteItem) => {
+      return EDLocData.filter(
+        (entry: EDLoc) => entry["eWaste_id"] === item["eWaste_id"]
+      ).map((entry: EDLoc) => {
+        return donateLocData[entry["donate_id"] - 1];
+      });
+    });
+    repairEWasteResults = repairEWaste.map((item: EWasteItem) => {
+      return ERLocData.filter(
+        (entry: ERLoc) => entry["eWaste_id"] === item["eWaste_id"]
+      ).map((entry: ERLoc) => {
+        return repairLocData[entry["repair_id"] - 1];
+      });
+    });
   }
-  useEffect(() => getResults());
 
   const handleBackClick = () => {
     setStage(2);
@@ -145,9 +161,15 @@ function ResultsPage({
           <Box flex={1}>
             <h2>Recyclables</h2>
             <h4>These items can be recycled at the nearest blue bins:</h4>
-            {recyclablesResults.map((item: RecyclableItem) => {
-              return <Typography variant="body1">{item["name"]}</Typography>;
-            })}
+            <ul>
+              {recyclablesResults.map((item: RecyclableItem) => {
+                return (
+                  <li>
+                    <Typography variant="body1">{item["name"]}</Typography>
+                  </li>
+                );
+              })}
+            </ul>
           </Box>
         )}
         {(goodDonatables.length != 0 || repairDonatables.length != 0) && (
@@ -155,19 +177,33 @@ function ResultsPage({
             <h2>Donatables</h2>
             {goodDonatables.length != 0 && (
               <>
-                <h4>Here's where to donate the following donatables</h4>
+                <h4>These donatables can be donated:</h4>
                 {goodDonatables.map((item: DonatableItem, index: number) => {
                   return (
                     <div>
                       <Typography variant="body1">
                         {item["donatable_type"] + ": "}
                       </Typography>
-                      <ul>
-                        <li>test</li>
-                        {/* {goodDonatablesResults[index].map((location: any) => {
-                    return <li>hi</li>;
-                  })} */}
-                      </ul>
+                      {goodDonatablesResults[index].length === 0 ? (
+                        <p>
+                          Oops, no locations found for {item["donatable_type"]}
+                        </p>
+                      ) : (
+                        <ul>
+                          {goodDonatablesResults[index].map(
+                            (location: DonateLocation) => {
+                              return (
+                                <li key={location["donate_id"]}>
+                                  <Typography>
+                                    {location["organisation_name"]}, located at{" "}
+                                    {location["address"]}
+                                  </Typography>
+                                </li>
+                              );
+                            }
+                          )}
+                        </ul>
+                      )}
                     </div>
                   );
                 })}
@@ -175,12 +211,36 @@ function ResultsPage({
             )}
             {repairDonatables.length != 0 && (
               <>
-                <h4>Here's where to repair the following donatables</h4>
-                {repairDonatables.map((item: DonatableItem) => (
-                  <Typography variant="body1">
-                    {item["donatable_type"]}
-                  </Typography>
-                ))}
+                <h4>These donatables can be repaired:</h4>
+                {repairDonatables.map((item: DonatableItem, index: number) => {
+                  return (
+                    <div>
+                      <Typography variant="body1">
+                        {item["donatable_type"] + ": "}
+                      </Typography>
+                      {repairDonatablesResults[index].length === 0 ? (
+                        <p>
+                          Oops, no locations found for {item["donatable_type"]}
+                        </p>
+                      ) : (
+                        <ul>
+                          {repairDonatablesResults[index].map(
+                            (location: RepairLocation) => {
+                              return (
+                                <li key={location["repair_id"]}>
+                                  <Typography>
+                                    {location["center_name"]}, stall number{" "}
+                                    {location["stall_number"]}
+                                  </Typography>
+                                </li>
+                              );
+                            }
+                          )}
+                        </ul>
+                      )}
+                    </div>
+                  );
+                })}
               </>
             )}
           </Box>
@@ -191,24 +251,68 @@ function ResultsPage({
               <h2>EWaste</h2>
               {goodEWaste.length != 0 && (
                 <>
-                  <h4>Here's where to donate the following EWaste</h4>
-                  {goodEWaste.map((item: EWasteItem) => {
+                  <h4>These EWaste items can be donated:</h4>
+                  {goodEWaste.map((item: EWasteItem, index: number) => {
                     return (
-                      <Typography variant="body1">
-                        {item["eWaste_type"]}
-                      </Typography>
+                      <div>
+                        <Typography variant="body1">
+                          {item["eWaste_type"] + ": "}
+                        </Typography>
+                        {goodEWasteResults[index].length === 0 ? (
+                          <p>
+                            Oops, no locations found for {item["eWaste_type"]}
+                          </p>
+                        ) : (
+                          <ul>
+                            {goodEWasteResults[index].map(
+                              (location: DonateLocation) => {
+                                return (
+                                  <li key={location["donate_id"]}>
+                                    <Typography>
+                                      {location["organisation_name"]}, located
+                                      at {location["address"]}
+                                    </Typography>
+                                  </li>
+                                );
+                              }
+                            )}
+                          </ul>
+                        )}
+                      </div>
                     );
                   })}
                 </>
               )}
               {repairEWaste.length != 0 && (
                 <>
-                  <h4>Here's where to repair the following EWaste</h4>
-                  {repairEWaste.map((item: EWasteItem) => {
+                  <h4>These EWaste items can be repaired:</h4>
+                  {repairEWaste.map((item: EWasteItem, index: number) => {
                     return (
-                      <Typography variant="body1">
-                        {item["eWaste_type"]}
-                      </Typography>
+                      <div>
+                        <Typography variant="body1">
+                          {item["eWaste_type"] + ": "}
+                        </Typography>
+                        {repairEWasteResults[index].length === 0 ? (
+                          <p>
+                            Oops, no locations found for {item["eWaste_type"]}
+                          </p>
+                        ) : (
+                          <ul>
+                            {repairEWasteResults[index].map(
+                              (location: RepairLocation) => {
+                                return (
+                                  <li key={location["repair_id"]}>
+                                    <Typography>
+                                      {location["center_name"]}, stall number{" "}
+                                      {location["stall_number"]}
+                                    </Typography>
+                                  </li>
+                                );
+                              }
+                            )}
+                          </ul>
+                        )}
+                      </div>
                     );
                   })}
                 </>
@@ -223,21 +327,43 @@ function ResultsPage({
         <>
           <h2>These items need to be disposed as general waste:</h2>
           <Stack>
-            {unrecyclablesResults.map((item: RecyclableItem) => {
-              return <Typography variant="body1">{item["name"]}</Typography>;
-            })}
-            {spoiltDonatables.map((item: DonatableItem) => {
-              return (
-                <Typography variant="body1">
-                  {item["donatable_type"]}
-                </Typography>
-              );
-            })}
-            {spoiltEWaste.map((item: EWasteItem) => {
-              return (
-                <Typography variant="body1">{item["eWaste_type"]}</Typography>
-              );
-            })}
+            {unrecyclablesResults.length != 0 && (
+              <>
+                <h3>
+                  (The following items may be of recyclable material, but its
+                  usage renders them unrecyclable:)
+                </h3>
+                {unrecyclablesResults.map((item: RecyclableItem) => {
+                  return (
+                    <Typography variant="body1">{item["name"]}</Typography>
+                  );
+                })}
+              </>
+            )}
+            {spoiltDonatables.length != 0 && (
+              <>
+                <h3>(Spoilt Donatables:)</h3>
+                {spoiltDonatables.map((item: DonatableItem) => {
+                  return (
+                    <Typography variant="body1">
+                      {item["donatable_type"]}
+                    </Typography>
+                  );
+                })}
+              </>
+            )}
+            {spoiltEWaste.length != 0 && (
+              <>
+                <h3>(Spoilt EWaste)</h3>
+                {spoiltEWaste.map((item: EWasteItem) => {
+                  return (
+                    <Typography variant="body1">
+                      {item["eWaste_type"]}
+                    </Typography>
+                  );
+                })}
+              </>
+            )}
           </Stack>
         </>
       )}
@@ -251,3 +377,22 @@ function ResultsPage({
 }
 
 export default ResultsPage;
+
+// Initial method of retrieving data by making Supabase call, but this returns a Promise object
+// instead of a DonateLocations[][] object, so doesn't work. Maybe will look into this again in future.
+//  goodDonatablesResults = goodDonatables.map(async (item: DonatableItem) => {
+//       const { data, error } = await supabase
+//         .from("DonatablesDonateLocations")
+//         .select()
+//         .match({ donatable_id: item["donatable_id"] });
+//       const locations =
+//         data === null
+//           ? []
+//           : data.map(async (entry) => {
+//               await supabase
+//                 .from("donateLocations")
+//                 .select()
+//                 .match({ donate_id: entry["donate_id"] });
+//             });
+//       return locations;
+//     });
