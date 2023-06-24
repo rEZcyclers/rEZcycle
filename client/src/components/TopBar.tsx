68bbe13cabd6f200acd285ef0a92195c62318507
jh <e0969121@u.nus.edu>
2023-06-24 11:14:21 +0800

    Modify Topbar styling

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -26,6 +26,7 @@ function TopBar() {
   return (
     <div className="TopBar">
       <Toolbar variant="dense">
+        <img src={logo} height={35} />
         <Typography
           variant="h5"
           noWrap
@@ -35,13 +36,11 @@ function TopBar() {
             paddingLeft: 3,
             display: "flex",
             alignItems: "center",
-            margin: 0,
+            marginRight: 5,
           }}
-        >
-          <img src={logo} height={35} />
-        </Typography>
-        <Box sx={{ color: "white", marginRight: "2%" }}>
-          Data Source:{" "}
+        ></Typography>
+        <Typography sx={{ color: "white", marginRight: "3%" }}>
+          Source:{" "}
           <a
             href="https://www.nea.gov.sg/our-services/towards-zero-waste"
             target="_blank"
@@ -49,7 +48,7 @@ function TopBar() {
           >
             NEA
           </a>
-        </Box>
+        </Typography>
         <Button
           variant="text"
           sx={{ color: "white" }}
