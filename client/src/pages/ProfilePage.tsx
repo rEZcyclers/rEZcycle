891fc06dc3f4a0a9ed76b9c976060f1dfbb4f134
jh <e0969121@u.nus.edu>
2023-06-30 00:00:44 +0800

    Modify ProfilePage to render ProfileCard

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,9 +1,22 @@
 import Base from "../components/Base";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { backendContext } from "../App";
+import ProfileCard from "./ProfileCard";
 
 function ProfilePage() {
   const { userSession, userProfile } = useContext(backendContext);
+  const [editMode, setEditMode] = useState<boolean>(false);
+  // TODO: Set edit modes for every info entry, which will be
+  // set to edit mode by an 'edit button'. Once that entry is
+  // in edit mode, it will render a separate component, which
+  // is an input field, a save button (which updates the database),
+  // and a cancel button which sets the edit mode for that entry
+  // back to false, rendering the original display as usual.
+
+  // TODO: Allow for user to customise their profile pic by
+  // uploading an image, which is then hosted as some link or sth?
+  // Dk how Supabase stores images if that's even possible.
+
   return (
     <Base>
       {userSession == null ? (
@@ -13,7 +26,14 @@ function ProfilePage() {
           {userProfile == null ? (
             <h1>Loading...</h1>
           ) : (
-            <h1>Welcome back, {userSession["user"]["email"]!}</h1>
+            <>
+              <h1>Welcome back, {userSession["user"]["email"]!}</h1>
+              {editMode ? (
+                <h1>Edit Mode</h1>
+              ) : (
+                <ProfileCard userProfile={userProfile} />
+              )}
+            </>
           )}
         </>
       )}
