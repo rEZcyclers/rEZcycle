ad812152a1eb33a49db9af81d2e91c045d3a4a29
jh <e0969121@u.nus.edu>
2023-07-07 01:21:12 +0800

    Trivial edit

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -5,17 +5,6 @@ import ProfileCard from "../components/ProfileCard";
 
 function ProfilePage() {
   const { serverAPI, userSession, userProfile } = useContext(backendContext);
-  // const [editMode, setEditMode] = useState<boolean>(false);
-  // TODO: Set edit modes for every info entry, which will be
-  // set to edit mode by an 'edit button'. Once that entry is
-  // in edit mode, it will render a separate component, which
-  // is an input field, a save button (which updates the database),
-  // and a cancel button which sets the edit mode for that entry
-  // back to false, rendering the original display as usual.
-
-  // TODO: Allow for user to customise their profile pic by
-  // uploading an image, which is then hosted as some link or sth?
-  // Dk how Supabase stores images if that's even possible.
 
   return (
     <Base>
