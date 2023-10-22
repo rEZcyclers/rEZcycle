51e56590da00f748c70e5d9a009b947d8373f0a3
J-hta-n <103252521+J-hta-n@users.noreply.github.com>
2023-10-22 21:14:24 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,7 +29,8 @@ import { createTheme } from "@mui/material";
 
 // backendContext object allows for sharing of state globally as seen later on
 export const backendContext = createContext<any>(null);
-const serverAPI = "https://rezcycle-server.onrender.com";
+const serverAPI = "https://rezcycle-dev-fzbt.2.sg-1.fl0.io/";
+// const serverAPI = "https://rezcycle-server.onrender.com";
 // const serverAPI = "http://localhost:8000";
 
 function App() {
